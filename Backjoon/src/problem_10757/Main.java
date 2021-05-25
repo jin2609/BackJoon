18774f9a84fb57eb08df1e3031378a916214f1e3
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-05-26 00:57:48 +0900

    Update Main.java
    add comment

--- Backjoon/src/problem_10757/Main.java
+++ Backjoon/src/problem_10757/Main.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 //시간 초과
 public class Main {
 	public static void main(String[] args) {
+		//변수생성
 		Scanner sc = new Scanner(System.in);
 		String A = sc.next();
 		String B = sc.next();
 		boolean rounding = false;
 		String result = "";
 		
+		//자리수 맞춰주기 
 		if(A.length() > B.length()) {
 			for(int i = B.length(); i < A.length(); i ++) {
 				A = '0' + A;
@@ -22,6 +24,7 @@ public class Main {
 			}
 		}
 		
+		//자리 덧셈 연산
 		for(int i = A.length()-1; i >= 0; i--) {
 			int add = Integer.parseInt(A.charAt(i)+"") + Integer.parseInt(B.charAt(i)+"");
 			if(rounding) {
@@ -37,6 +40,7 @@ public class Main {
 			}
 		}
 		
+		//마지막 결과 반올림
 		if(rounding)
 			System.out.print(1);
 		
